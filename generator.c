6443cdadae372da4520e5d29ababd66f8d03c5d6
groot <guyanbiao@gmail.com>
2018-09-13 15:22:15 +0800

    function return str

--- generator.c
+++ generator.c
@@ -10,40 +10,36 @@ bool generate_totp() {
 	uint32_t bin_code, totp;
 	uint8_t * key, *result;
   time_t current_time;
-  current_time = time(NULL);
-  char hex[8];
-  printf("ooooooooooooooo");
-  sprintf(hex, "%x", current_time / 60);
-  printf("--------------------%s\n", hex);
-  printf("--------------------%d\n", sizeof(hex));
-	result = hmac_sha512( "hello", 5,  hex, 7 /* 8 */);
-	printf("\n\n\tresult: %s\n\n", result);
-	printf("\n\n\tresult: %d\n\n", result);
+  current_time = time(NULL) / PERIOD;
+  unsigned char secret[] = SECRET;
+  unsigned char *data = target_data(current_time);
+	result = hmac_sha512(secret, sizeof(secret), data, 8);
+  free(data);
 	bin_code = dynamic_truncation( result, RESULT_LEN);
-  printf("\dbin_code:%d\n", bin_code);
   int sss = pow(10, DIGITS);
-  printf("\ddd: %d\n", DIGITS);
-  printf("\nsss: %d\n", sss);
 	totp = bin_code % sss;
-	printf("\n\n\tOne-time Password: %u\n\n", totp);
+	printf("One-time Password: %d\n", totp);
 	return true;
 }
 
+unsigned char * target_data(time_t current_time) {
+  char shex[9];
+  unsigned char *str = (unsigned char *) malloc(sizeof(char) * 8);
+  if (shex[7] == 0) {
+    memcpy(str, "00000000", sizeof(char) * 8);
+    memcpy(str + 1, shex, sizeof(char) * 7);
+  } else {
+    memcpy(str, shex, sizeof(char) * 8);
+  }
+  return str;
+}
 
 uint32_t dynamic_truncation(const uint8_t *input, int length)
 {
 	uint32_t bin_code;
 	uint8_t offset;
-
 	offset = input[length - 1] & 0x3F;
 	offset = offset % MAX_OFFSET;  
-
-  printf("\ntest:%d\n", input[offset]);
-  printf("\ntest:%d\n", input[offset + 1]);
-  printf("\ntest:%d\n", input[offset + 2]);
-  printf("\ntest:%d\n", input[offset + 3]);
-
-
 	bin_code = (input[offset] & 0x7f) << 24
 		| (input[offset+1] & 0xff) << 16
 		| (input[offset+2] & 0xff) << 8
--- generator.h
+++ generator.h
@@ -1,36 +1,4 @@
-/**
- * @file
- * @author Sean Easton <gonrada@gmail.com>
- *
- * @section COURSE
- *
- * Course Information: CIS5370 - Fall '12 <br/>
- * Due Date: December 14, 2012 <br/>
- *
- * @section LICENSE
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details at
- * http://www.gnu.org/copyleft/gpl.html
- *
- * @section DESCRIPTION
- *
- * A C implementation of a Time-base One-Time Password generator using openssl
- */
-
-
-#ifndef __GENERATOR_H
-#define __GENERATOR_H
-
 #include "hmac.h"
-
 #include <inttypes.h>
 #include <math.h>
 #include <openssl/bio.h>
@@ -54,49 +22,13 @@
 #define PRINTD( format, ... )
 #endif
 
-#define CONFIG_FILE ".totp_conf"
-#define KEY_FILE "totp.key"
 #define DIGITS 6
-#define PERIOD 30
-#define SEED_LEN 64
+#define PERIOD 60
 #define MAX_OFFSET (length - 5)
+#define SECRET "your_secret"
 
-
-/**
- *
- *
- */
-bool check_configuration(void);
-
-/**
- *
- *
- */
-bool generate_config(void);
-
-/**
- *
- */
 bool generate_totp(void);
 
-/**
- *
- * http://www.ioncannon.net/programming/34/howto-base64-encode-with-cc-and-openssl/
- */
-uint8_t * base64_encode(const uint8_t *input, int length);
-
-/**
- *
- * http://www.ioncannon.net/programming/34/howto-base64-encode-with-cc-and-openssl/
- */
-uint8_t * base64_decode(uint8_t *input, int length);
-
-/**
- *
- *
- */
 uint32_t dynamic_truncation(const uint8_t *input, int length);
 
-
-#endif
-
+unsigned char* target_data(time_t current_time);
